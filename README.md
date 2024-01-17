a67f27ab640e6e779ee720cdf4e42f9792aec098
chen.tao <chentao0204@qq.com>
2024-01-17 15:53:50 +0800

    use table rendering domians list

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 在本地局域网中运行此程序。此程序会查询本地局域网所属的公网IP（网络供应商分配的动态IP）， 然后通过阿里云API，将该IP绑定到阿里云指定的域名上，从而实现域名与IP的动态绑定。
 
 
+[API参考文档](https://help.aliyun.com/document_detail/2355673.html?spm=a2c4g.2355665.0.0.60734802Zanhco)
+
+
 
 AccessKey ID
 LTAI5tDJApv8zLPf99BAFam9
--- cmd/query_domains.go
+++ cmd/query_domains.go
@@ -2,39 +2,62 @@ package cmd
 
 import (
 	"dynamic_ip/pkg"
+	"fmt"
+	"net/http"
+	"os"
 
+	"github.com/alibabacloud-go/alidns-20150109/v4/client"
 	alidns20150109 "github.com/alibabacloud-go/alidns-20150109/v4/client"
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 )
 
 func init() {
-    rootCmd.AddCommand(queryDomainsCmd)
+	rootCmd.AddCommand(queryDomainsCmd)
 }
 
 var queryDomainsCmd = &cobra.Command{
-    Use: "queryDomains",
-    Short: "Query domains from Aliyun",
-    RunE: queryDomainsCmdFunc,
+	Use:   "queryDomains",
+	Short: "Query domains from Aliyun",
+	RunE:  queryDomainsCmdFunc,
 }
 
 func queryDomainsCmdFunc(cmd *cobra.Command, args []string) error {
-  println("query domains")
-  describeDomainsRequest := &alidns20150109.DescribeDomainsRequest{}
-  runtime := &util.RuntimeOptions{}
-  tryErr := func(client *alidns20150109.Client)(_e error) {
-    // 复制代码运行请自行打印 API 的返回值
-    result, _err := client.DescribeDomainsWithOptions(describeDomainsRequest, runtime)
-    if _err != nil {
-      return _err
-    }
+	describeDomainsRequest := &alidns20150109.DescribeDomainsRequest{}
+	runtime := &util.RuntimeOptions{}
+	domains := &client.DescribeDomainsResponseBodyDomains{}
+	tryErr := func(client *alidns20150109.Client) error {
+		// 复制代码运行请自行打印 API 的返回值
+		result, _err := client.DescribeDomainsWithOptions(describeDomainsRequest, runtime)
+		if _err != nil {
+			return _err
+		}
 
-    println(result.String())
+		if *result.StatusCode != http.StatusOK {
+			return fmt.Errorf("error with http status code: %d", *result.StatusCode)
+		}
 
-    return nil
-  }
+		domains = result.Body.Domains
+		return nil
+	}
 
-  err := pkg.Invoke(tryErr)
+	err := pkg.Invoke(tryErr)
+	if err != nil {
+		return err
+	}
 
-  return err
+	// print domains
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"#", "Domain Id", "Domain Name", "Record Count", "Create Time"})
+
+	for idx, domain := range domains.Domain {
+		t.AppendRow(table.Row{idx, *domain.DomainId, *domain.DomainName, *domain.RecordCount, *domain.CreateTime})
+		t.AppendSeparator()
+	}
+
+	t.Render()
+
+	return nil
 }
